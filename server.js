2fe6833d5dd8686c9bc4d1e3fa2143cfbf768ced
Bhanu <mobiweb.developer2@gmail.com>
2025-03-24 15:55:45 +0530

    Fixed MongoDB connection issue

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+.catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 // Lead Schema
 const LeadSchema = new mongoose.Schema({
