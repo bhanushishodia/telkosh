43752dfdf75559c265acce0da86e334e767b4e68
Bhanu <mobiweb.developer2@gmail.com>
2025-05-05 12:20:23 +0530

    Updated SMS service to use CountryCode=All and dynamic sender IDs

--- services/smsService.js
+++ services/smsService.js
@@ -7,20 +7,19 @@ const sendSMSMessage = async (mobileno, msgtext) => {
   const user = '20083935';
   const pwd = 'neeraj';
 
-  // Detect country from mobile number
-  let senderid = 'MOALRT';
-  let countryCode = '91'; // Default to India
+  // Set senderid based on country
+  let senderid = 'MOALRT'; // Default sender ID
 
   if (mobileno.startsWith('+973')) {
-    senderid = 'Mobishtra'; // Bahrain sender ID
-    countryCode = '973';
+    senderid = 'Mobishtra'; // Bahrain
   } else if (mobileno.startsWith('+60')) {
-    senderid = 'Mobishtra'; // Malaysia sender ID
-    countryCode = '60';
+    senderid = 'Mobishtra'; // Malaysia
   } else if (mobileno.startsWith('+91')) {
-    senderid = 'MOALRT'; // India sender ID
-    countryCode = '91';
+    senderid = 'MOALRT'; // India
   }
+
+  // Always use "All" for CountryCode as per instruction
+  const countryCode = 'All';
 
   const url = `http://mshastra.com/sendurl.aspx?user=${encodeURIComponent(user)}&pwd=${encodeURIComponent(pwd)}&senderid=${encodeURIComponent(senderid)}&mobileno=${encodeURIComponent(mobileno)}&msgtext=${encodeURIComponent(msgtext)}&CountryCode=${encodeURIComponent(countryCode)}`;
 
